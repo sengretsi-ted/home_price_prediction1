6c1a8ad41a29851dac56f0c22e7af162cb6eeeba
Ted S <tsengretsi2@gmail.com>
2024-11-22 17:00:16 +0000

    installed pipreqs

--- app1.py
+++ app1.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import joblib
 
 # Title
 st.title("Home Price Prediction using Machine Learning")
@@ -84,18 +83,13 @@ if "predictions" not in st.session_state:
 # Load saved models and make predictions
 def load_model(filename):
     with open(filename, 'rb') as model_file:
-        return joblib.load(model_file)
+        return pickle.load(model_file)
 
 # Load saved models
 
-with open('rf_r.pkl', 'rb') as model_file:
-    rf_model = joblib.load(model_file)
-
 # Load Random forest 
-
-'''test'''
-# with open('rf_r.pkl', 'rb') as model_file:
-#     rf_model = pickle.load(model_file)
+with open('rf_r.pkl', 'rb') as model_file:
+    rf_model = pickle.load(model_file)
 # Assign model to variable
 random_forest_model_prediciton = rf_model.predict(df)[0]
 # Button to predict model
@@ -104,48 +98,48 @@ if st.button("Random Forest Model Prediction"):
     st.session_state.predictions["Random Forest"] = f"Predicted Median Value of House is: ${random_forest_model_prediciton:.2f}"
 
 
-# # Decision Tree model
-# with open('dt_r.pkl', 'rb') as model_file:
-#     dt_model = pickle.load(model_file)
-# # Assign model to variable
-# decision_tree_model_prediciton = dt_model.predict(df)[0]
-# # Button to predict model
-# if st.button("Decision Tree Model Prediction"):
-#     # Output model results
-#     st.session_state.predictions["Decision Tree"] = f"Predicted Median Value of House is: ${decision_tree_model_prediciton:.2f}"
+# Decision Tree model
+with open('dt_r.pkl', 'rb') as model_file:
+    dt_model = pickle.load(model_file)
+# Assign model to variable
+decision_tree_model_prediciton = dt_model.predict(df)[0]
+# Button to predict model
+if st.button("Decision Tree Model Prediction"):
+    # Output model results
+    st.session_state.predictions["Decision Tree"] = f"Predicted Median Value of House is: ${decision_tree_model_prediciton:.2f}"
 
 
-# # MLP model
-# with open('mlp_r.pkl', 'rb') as model_file:
-#     mlp_model = pickle.load(model_file)
-# # Assign model to variable
-# mlp_prediction = mlp_model.predict(df)[0]
-# # Button to predict model
-# if st.button("MLP Prediction"):
-#     # Output model results
-#     st.session_state.predictions["MLP"] = f"Predicted Median Value of House is: ${mlp_prediction:.2f}"
+# MLP model
+with open('mlp_r.pkl', 'rb') as model_file:
+    mlp_model = pickle.load(model_file)
+# Assign model to variable
+mlp_prediction = mlp_model.predict(df)[0]
+# Button to predict model
+if st.button("MLP Prediction"):
+    # Output model results
+    st.session_state.predictions["MLP"] = f"Predicted Median Value of House is: ${mlp_prediction:.2f}"
 
 
-# # Elastic Net model
-# with open('en_r.pkl', 'rb') as model_file:
-#     en_r_model = pickle.load(model_file)
-# # Assign model to variable
-# en_prediction = en_r_model.predict(df)[0]
-# # Button to predict model
-# if st.button("Elastic Net Prediction"):
-#     # Output model results
-#     st.session_state.predictions["Elastic Net"] = f"Predicted Median Value of House is: ${en_prediction:.2f}"
+# Elastic Net model
+with open('en_r.pkl', 'rb') as model_file:
+    en_r_model = pickle.load(model_file)
+# Assign model to variable
+en_prediction = en_r_model.predict(df)[0]
+# Button to predict model
+if st.button("Elastic Net Prediction"):
+    # Output model results
+    st.session_state.predictions["Elastic Net"] = f"Predicted Median Value of House is: ${en_prediction:.2f}"
 
 
-# # Multiplelinear Regression model
-# with open('lr_r.pkl', 'rb') as model_file:
-#     lr_model = pickle.load(model_file)
-# # Assign model to variable
-# lr_prediction = lr_model.predict(df)[0]
-# # Button to predict model
-# if st.button("Multiplelinear Regression Prediction"):
-#     # Output model results
-#     st.session_state.predictions["Linear Regression"] = f"Predicted Median Value of House is: ${lr_prediction:.2f}"
+# Multiplelinear Regression model
+with open('lr_r.pkl', 'rb') as model_file:
+    lr_model = pickle.load(model_file)
+# Assign model to variable
+lr_prediction = lr_model.predict(df)[0]
+# Button to predict model
+if st.button("Multiplelinear Regression Prediction"):
+    # Output model results
+    st.session_state.predictions["Linear Regression"] = f"Predicted Median Value of House is: ${lr_prediction:.2f}"
 
 # Display all selected predictions
 st.title("Model Predictions")
