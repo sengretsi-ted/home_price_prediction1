d5585e8d10ae677d9bda6c6f45d1611396d1d61e
Ted S <tsengretsi2@gmail.com>
2024-11-23 01:48:53 +0000

    updated app file

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import onnxruntime as rt
-from skl2onnx import convert_sklearn
+# from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
 
 # Title
